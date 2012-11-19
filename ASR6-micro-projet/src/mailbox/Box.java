79ac72c8feb7b4417fd9e7331fe35fa7ea871565
wangtz <jwzxgo@hotmail.com>
2012-11-19 17:05:27 +0100

    cleanning

--- ASR6-micro-projet/src/mailbox/Box.java
+++ ASR6-micro-projet/src/mailbox/Box.java
@@ -1,57 +1,52 @@
 package mailbox;
 
-import static javax.persistence.CascadeType.ALL;
-
-import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.NoResultException;
-import javax.persistence.OneToMany;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import entity.Messageentity;
 
-
 @Stateless
 public class Box implements IBox {
-	
+
 	@PersistenceContext(unitName = "box1")
 	private EntityManager em;
-	
-	public Box(){}
-	
+
+	public Box() {
+	}
+
 	@Override
 	public Collection<Messageentity> listAllMessages(int id) {
-		 try{
-			 
-			 Query q=em.createQuery("select b.Messages from Boxentity b where b.id= :id");
-			 q.setParameter("id", id);
-			 Collection<Messageentity> resultList = (Collection<Messageentity>)q.getResultList();
+		try {
+
+			Query q = em
+					.createQuery("select b.Messages from Boxentity b where b.id= :id");
+			q.setParameter("id", id);
+			Collection<Messageentity> resultList = (Collection<Messageentity>) q
+					.getResultList();
 			return resultList;
-			 
-		 } catch (NoResultException e) {
-				return null;
+
+		} catch (NoResultException e) {
+			return null;
 		}
-	
-		// TODO Auto-generated method stub	
+
+		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void readAMessage(Messageentity msg) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addMessage(Messageentity msg) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }

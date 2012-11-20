13c3a0ca8c940eb9264010079c5eb9129b07e544
wangtz <jwzxgo@hotmail.com>
2012-11-20 12:37:56 +0100

    use default port 3700 explicitly

--- ASR6-micro-projet/src/application/FillDataClient.java
+++ ASR6-micro-projet/src/application/FillDataClient.java
@@ -1,7 +1,12 @@
 package application;
 
+import java.util.Properties;
+
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
+//import org.omg.CORBA.Context;<>
 
 import mailbox.IFillDataServer;
 
@@ -13,9 +18,17 @@ public class FillDataClient {
 	 */
 	public static void main(String[] args) throws NamingException {
 		// TODO Auto-generated method stub
-		InitialContext ic = new InitialContext();
-		IFillDataServer sb = (IFillDataServer) ic.lookup("mailbox.IFillDataServer");
-
+		Properties props = new Properties();
+		props.put(Context.INITIAL_CONTEXT_FACTORY,
+				"com.sun.enterprise.naming.SerialInitContextFactory");
+		props.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+		props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+		
+		InitialContext ic = new InitialContext(props);
+		IFillDataServer sb = (IFillDataServer) ic
+				.lookup("mailbox.IFillDataServer");
+		System.out.println("lookup [OK]");
+		
 		sb.fillData();
 		System.out.println("c'est la fin");
 	}
